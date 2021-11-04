00df8d16e8771a55bfb01a075c1192083b840335
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-11-04 17:33:36 +0100

    pass tests

--- cmd.go
+++ cmd.go
@@ -1,6 +1,12 @@
 package blog
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"path"
+
+	"github.com/spf13/afero"
+)
 
 type Metadata struct {
 	Title string
@@ -14,4 +20,25 @@ title: %s
 categories: %v
 date: %s
 ---`,m.Title,m.Categories,m.Date)
+}
+
+type File struct {
+	Title string
+	Path string
+}
+
+
+func (f File) filepath() string {
+	return path.Join(f.Path,f.Title+".md")
+}
+
+func (f File) Create(fsys afero.Fs) (afero.File, error) {
+	return fsys.Create(f.filepath())
+}
+
+type Blog struct {
+}
+
+func (b Blog) WritePost(metadata Metadata,file io.Writer) {
+	io.WriteString(file,metadata.String())
 }
--- cmd_test.go
+++ cmd_test.go
@@ -1,9 +1,11 @@
 package blog_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/elchead/blog-cli"
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 )
 func TestAddMetadata(t *testing.T) {
@@ -14,4 +16,30 @@ categories: [Thoughts]
 date: 2021-11-04
 ---`
 	assert.Equal(t,want,sut.String())
+}
+
+
+func TestCreateFile(t *testing.T) {
+	t.Run("save file",func(t *testing.T) {
+		sut := blog.File{Title: "title",Path:"blog-cli"}
+		appFS := afero.NewMemMapFs()
+		sut.Create(appFS)
+		exists, _ := afero.Exists(appFS,sut.Path+"/title.md")
+		assert.Equal(t,true,exists)
+	})
+	t.Run("non existent file is not saved", func(t *testing.T) {
+		sut := blog.File{Title: "title",Path:"blog-cli"}
+		appFS := afero.NewMemMapFs()
+		sut.Create(appFS)
+		exists, _ := afero.Exists(appFS,sut.Path+"nofile.md")
+		assert.Equal(t,false,exists)
+	})
+}
+
+func TestBlog(t *testing.T){
+	sut := blog.Blog{}
+	meta := blog.Metadata{Title: "title", Categories : []string{"Thoughts"}, Date: "2021-11-04"}
+	var file bytes.Buffer
+	sut.WritePost(meta,&file)
+	assert.Equal(t,meta.String(),file.String())
 }
